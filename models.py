65ae1e82a49e3bdb485bd31378fd304e59a4b5fa
trikyas <mooneychad@yahoo.com>
2016-12-05 18:06:00 +1100

    updated fix

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import sqlalchemy
 from werkzeug import generate_password_hash, check_password_hash
 
-db = sqlalchemy(app)
+db = sqlalchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
