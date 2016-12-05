2fb835881862b90bbf76129fc1ae7feba56d4b26
trikyas <mooneychad@yahoo.com>
2016-12-05 17:00:06 +1100

    connected db to application

--- routes.py
+++ routes.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'pastgressql://localhost/discuva'
+db.init_app(app)
 
 @app.route("/")
 def index():
