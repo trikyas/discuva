0639fea90ec52d55b5c2e6ebddee167552f566f1
trikyas <mooneychad@yahoo.com>
2016-12-06 01:50:43 +1100

    added logout

--- routes.py
+++ routes.py
@@ -58,6 +58,10 @@ def login():
   elif request.method == 'GET':
     return render_template('login.html', form=form)
 
+@app.route("/logout")
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
         
 @app.route("/home")
 def home():
