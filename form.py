b00f16c414bc77618d8fbe0afb564a5d7beea652
trikyas <mooneychad@yahoo.com>
2016-12-05 23:01:24 +1100

    added length of password input to 7 characters or more

--- form.py
+++ form.py
@@ -1,10 +1,10 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
     first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
     last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
     email = StringField('email', validators=[DataRequired("Please enter your email address."), Email("Please enter your correct email address.")])
-    password = PasswordField('password', validators=[DataRequired("Please enter a password.")])
+    password = PasswordField('password', validators=[DataRequired("Please enter a password."), Length(min=7, message="Passwords must be 7 characters or more.")])
     submit = SubmitField('Sign up')
