ee0fa9cd11bbd1bc2fd7b393be3fd7e437e2110f
trikyas <mooneychad@yahoo.com>
2016-12-05 22:51:25 +1100

    Changed error messages to match inputs

--- form.py
+++ form.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class SignupForm(Form):
-    first_name = StringField('First name', validators=[DataRequired()])
-    last_name = StringField('Last name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
+    last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
+    email = StringField('email', validators=[DataRequired("Please enter your email address.")])
+    password = PasswordField('password', validators=[DataRequired("Please enter a password.")])
     submit = SubmitField('Sign up')
