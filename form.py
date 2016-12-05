63ef71a43db936a7873dd2d6893f5fb51967ff80
trikyas <mooneychad@yahoo.com>
2016-12-05 17:18:01 +1100

    created signup page

---
+++ form.py
@@ -0,0 +1,9 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+
+class signupForm(Form):
+    first_name = StringField('First name')
+    last_name = StringField('Last name')
+    email = StringField('email')
+    password = PasswordField('password')
+    submit = SubmitField('Sign up')
--- routes.py
+++ routes.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from models import db
+from forms import signupForm
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'pastgressql://localhost/discuva'
 db.init_app(app)
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
@@ -13,6 +15,11 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/signup")
+def signup():
+    form = signupForm()
+    return render_template("signup.html", form=form)
     
-    if __name__ == "__main__":
-        app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
