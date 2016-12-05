cb4ea2cebb0c0212dd72970234c17f126ca2c0d8
trikyas <mooneychad@yahoo.com>
2016-12-05 15:08:12 +1100

    Still building App
    Update-2

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/learning-flask/venv"
+set -gx VIRTUAL_ENV "/discuva/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
